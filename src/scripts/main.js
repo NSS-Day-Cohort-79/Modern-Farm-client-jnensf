7bdabef6a47390a6e56c0534131695a4d0ce53af
Larissa Ferreira <larissadferr@gmail.com>
2025-11-20 17:08:42 -0300

    invoked tractor's plantSeeds in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,10 @@
+import { plantSeeds } from "./tractor"
+
 console.log("Welcome to the main module")
 
+// tractor
+// invoke plant seeds from tractor
+const tractorPlantSeeds = plantSeeds(yearlyPlan)
+
+// invoke use seeds from field
+const getPlantsFields = usePlants()
