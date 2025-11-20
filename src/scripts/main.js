f627bc73864b1428834746a6e0a24a17564a19fa
Larissa Ferreira <larissadferr@gmail.com>
2025-11-20 17:10:17 -0300

    invoke createPlan from plan into main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,14 @@
+import { createPlan } from "./plan"
 import { plantSeeds } from "./tractor"
 
 console.log("Welcome to the main module")
 
+// invoke createPlan from Plan
+const yearlyPlan = createPlan()
+
 // tractor
 // invoke plant seeds from tractor
-const tractorPlantSeeds = plantSeeds(yearlyPlan)
+plantSeeds(yearlyPlan)
 
 // invoke use seeds from field
 const getPlantsFields = usePlants()
