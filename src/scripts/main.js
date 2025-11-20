28b7d566e70f77ea4c1666d2520bffc1a5c0fb69
Larissa Ferreira <larissadferr@gmail.com>
2025-11-20 18:11:55 -0300

    merge - imports added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,22 @@
 import { createPlan } from "./plan.js"
 import { plantSeeds } from "./tractor.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { usePlants } from "./field.js"
 
 console.log("Welcome to the main module")
 
 // invoke createPlan from Plan
 const yearlyPlan = createPlan()
+
+//create a seed
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
+
+addPlant(asparagusSeed)
+let fieldPlants = usePlants()
+
+console.log(fieldPlants)
 
 // tractor
 // invoke plant seeds from tractor
